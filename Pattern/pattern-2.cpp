a0d9c99f2f97da2cd93306d8b590533265ac29c8
Adityashah2003 <adigshah14@gmail.com>
2022-12-29 12:21:12 +0530

    updated

---
+++ Pattern/pattern-2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i=0;
+    int j=0;
+    int n=5;
+
+    for(i=0;i<n;i++){
+        cout<<endl;
+        for(j=0;j<=i;j++){
+            cout<<'*';
+        }
+    }
+}
---
+++ Pattern/pattern-4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i=0;
+    int j=0;
+    int n=5;
+
+    for(i=1;i<n+1;i++){
+        cout<<endl;
+        for(j=1;j<=i;j++){
+            cout<<i;
+        }
+    }
+}
